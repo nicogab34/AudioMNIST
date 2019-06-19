be7f425c4a3c52e44ddb2efca58034b3b025b596
Julien Duquesne <ju.duquesne@hotmail.fr>
2019-06-19 15:11:52 +0200

    visualize dense layers

--- bash/train_alexnet.sh
+++ bash/train_alexnet.sh
@@ -3,10 +3,10 @@
 #PBS -S /bin/bash
 #PBS -N ELMo-test
 #PBS -l walltime=2:00:00
-#PBS -l select=1:ncpus=2:gputype=p100
+#PBS -l select=2:ncpus=2:gputype=p100
 #PBS -q gpuq
-#PBS -P randstad
-#PBS -M badryoubiidrissi@gmail.com
+#PBS -P aud_interp
+#PBS -M julien.duquesne@student.ecp.fr
 #PBS -o logs/output.txt
 #PBS -e logs/error.txt
 
@@ -15,8 +15,8 @@
 module load cuda/10.0
 module load cudnn/7.4.2
 
-cd /workdir/idrissib/AudioMNIST
+cd /workdir/2017duquesnej/AudioMNIST
 
 source activate aud_interp_gpu
 
-mprof run -o "logs/mprofile_<YYYYMMDDhhmmss>.dat" train_alexnet.py -i tf_data/alexnet.tfrecords -o models/alexnet2 -l tensorboard/alexnet2_lr_0.0005 -b 100 -e 50
+mprof run -o "logs/mprofile_<YYYYMMDDhhmmss>.dat" train_alexnet.py -i tf_data/alexnet.tfrecords -o models/alexnet3 -l tensorboard/alexnet_lr_0.0005 -b 100 -e 50
